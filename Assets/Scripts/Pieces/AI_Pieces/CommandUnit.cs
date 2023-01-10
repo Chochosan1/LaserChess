c4b1410330caf05d8bcd99af1be9ae70960441e9
Chochosan1 <46068526+Chochosan1@users.noreply.github.com>
2023-01-10 14:10:26 +0200

    Command unit avoidance (impossible to beat with 1 grunt)
    -Command unit will always avoid if there's a tank in direct line of sight on the current tile
    -If not, it will try to move left/right. But it will try to avoid the following: a tank in line of sight of the new left/right tile  AND a grunt in the CURRENT diagonals. If these can't be avoided (both of them) then it will stay in one place

--- Assets/Scripts/Pieces/AI_Pieces/CommandUnit.cs
+++ Assets/Scripts/Pieces/AI_Pieces/CommandUnit.cs
@@ -12,6 +12,7 @@ public class CommandUnit : Piece, IAutoRunnableAI
     private GridTile currentGridTileToMoveTo;
     private float step;
     private bool isActivatedAndMustPlay = false; //should the AI behaviour logic execute?
+    private List<GridTile> checkForEnemiesToAvoidPath = new List<GridTile>();
 
     protected override void Start()
     {
@@ -69,25 +70,103 @@ public class CommandUnit : Piece, IAutoRunnableAI
         isActivatedAndMustPlay = true;
         hasPlayedItsTurn = false;
 
-        int direction = Random.Range(0, 2);
+        currentGridTileToMoveTo = null;
+        bool threatFound = false;
 
+        //Prio 1: check for tanks on the bottom orgthogonal path and move left/right (avoids staying in the direct line of fire)
+        //(top is not possible as command units stay always on last top row and they can only move left/right so left/right/top tank checking is useless)
+        checkForEnemiesToAvoidPath = MapController.Instance.GetPossibleRouteFromTile(StandingOnTile, 10, MapController.Directions.Bot, true);
+        foreach (GridTile tile in checkForEnemiesToAvoidPath)
+        {
+            if(tile.BlockingTilePiece != null && tile.BlockingTilePiece.GetComponent<Tank>() != null)
+            {
+                OnMoveCommand(GetRandomLeftOrRightTile());
+                return;
+            }
+        }
+
+        //Prio 2: Try moving left/right if there's no tank in direct line of sight on the new tile. Also, there should be no grunts on the CURRENT diagonals (because if they are 
+        //in the current diagonals they would need at least 2 rounds to attack, whereas if the command unit moves it may reduce that number to only 1 round). This makes it
+        //essentially impossible to win with only 1 grunt vs the command unit
+        GridTile currentNextTile = StandingOnTile.LeftNeighbour;
+        for(int i = 0; i < 2; i++)
+        {
+            threatFound = false;
+            if (i == 1)
+                currentNextTile = StandingOnTile.RightNeighbour;
+
+            if (currentNextTile != null)
+            {
+                //if there's a tank in direct line of sight on the new tile then just abandon
+                checkForEnemiesToAvoidPath = MapController.Instance.GetPossibleRouteFromTile(currentNextTile, 10, MapController.Directions.Bot, true);
+                foreach (GridTile tile in checkForEnemiesToAvoidPath)
+                {
+                    if (tile.BlockingTilePiece != null && tile.BlockingTilePiece.GetComponent<Tank>() != null)
+                    {
+                        threatFound = true;
+                        Debug.Log("Tank threat found!");
+                        break;
+                    }
+                }
+
+                //if there's a grunt on the current left diagonal then just abandon (else it can just move orthogonally one tile next round and attack directly, whereas
+                //not moving will have the grunt lose at least 1 round not attacking)
+                checkForEnemiesToAvoidPath = MapController.Instance.GetPossibleRouteFromTile(StandingOnTile, 10, MapController.Directions.BotLeft, true);
+                foreach (GridTile tile in checkForEnemiesToAvoidPath)
+                {
+                    if (tile.BlockingTilePiece != null && tile.BlockingTilePiece.GetComponent<Grunt>() != null)
+                    {
+                        threatFound = true;
+                        Debug.Log("Grunt botleft threat found!");
+                        break;
+                    }
+                }
+
+                //if there's a grunt on the current right diagonal then just abandon (else it can just move orthogonally one tile next round and attack directly, whereas
+                //not moving will have the grunt lose at least 1 round not attacking)
+                checkForEnemiesToAvoidPath = MapController.Instance.GetPossibleRouteFromTile(StandingOnTile, 10, MapController.Directions.BotRight, true);
+                foreach (GridTile tile in checkForEnemiesToAvoidPath)
+                {
+                    if (tile.BlockingTilePiece != null && tile.BlockingTilePiece.GetComponent<Grunt>() != null)
+                    {
+                        threatFound = true;
+                        Debug.Log("Grunt botright threat found!");
+                        break;
+                    }
+                }
+
+                if (!threatFound)
+                {
+                    OnMoveCommand(currentNextTile);
+                    return;
+                }
+            }
+        }
+    }
+
+    private GridTile GetRandomLeftOrRightTile()
+    {
+        GridTile gridTileChosen = null;
+        int direction = Random.Range(0, 2);
         //pick randomly to move left or right 
         if (direction == 0)
         {
             //prioritize left move; if not available go right
             if (StandingOnTile.LeftNeighbour != null && !StandingOnTile.LeftNeighbour.IsBlocked)
-                OnMoveCommand(StandingOnTile.LeftNeighbour);
+                gridTileChosen = StandingOnTile.LeftNeighbour;      
             else if (StandingOnTile.RightNeighbour != null && !StandingOnTile.RightNeighbour.IsBlocked)
-                OnMoveCommand(StandingOnTile.RightNeighbour);
-
+                gridTileChosen = StandingOnTile.RightNeighbour;
         }
         else if (direction == 1)
         {
+            //prioritize right move; if not available go left
             if (StandingOnTile.RightNeighbour != null && !StandingOnTile.RightNeighbour.IsBlocked)
-                OnMoveCommand(StandingOnTile.RightNeighbour);
+                gridTileChosen = StandingOnTile.RightNeighbour;    
             else if (StandingOnTile.LeftNeighbour != null && !StandingOnTile.LeftNeighbour.IsBlocked)
-                OnMoveCommand(StandingOnTile.LeftNeighbour);
+                gridTileChosen = StandingOnTile.LeftNeighbour;      
         }
+
+        return gridTileChosen;
     }
 
     public bool IsAutoRunDone() => hasPlayedItsTurn;
